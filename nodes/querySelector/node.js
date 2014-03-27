f0db826cd6e417d79a29577480c4a6143399497c
Rob Halff <rob.halff@gmail.com>
2014-03-27 02:50:01 +0100

    document

--- nodes/querySelector/node.js
+++ nodes/querySelector/node.js
@@ -1,4 +1,4 @@
-var el = input.element ? input.element : document.body;
+var el = input.element ? input.element : document;
 output = {
   element: el,
   selection: el.querySelector(input.selector)
