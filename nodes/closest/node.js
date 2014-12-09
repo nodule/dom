7f6b6c59d05789174d3ebac464f28f46adcf6eeb
Rob Halff <rob.halff@gmail.com>
2014-12-09 20:07:49 +0100

    output error

--- nodes/closest/node.js
+++ nodes/closest/node.js
@@ -1,9 +1,15 @@
 on.input.element = function(data) {
-  output({
-    selection: discore_closest(
-      data, input.selector, input.checkSelf,
-      input.within || document
-    ),
-    element: data
-  });
+  var sel = discore_closest(
+    data, input.selector, input.checkSelf,
+    input.within || document
+  );
+  var res = { element: data };
+  if (sel) {
+    res.selection = sel;
+  } else {
+    res.error = new Error('Selector didn\'t match:' + input.selector);
+  }
+
+  output(res);
+
 };
